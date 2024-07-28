4ac4f6e12487f8b916a029c1391e00227cc0f41b
David Bureš <buresdv@gmail.com>
2024-07-28 20:32:08 +0200

    + Swipe actions in sidebar

--- Cork/Views/Sidebar/Components/Sidebar Package Row.swift
+++ Cork/Views/Sidebar/Components/Sidebar Package Row.swift
@@ -7,20 +7,22 @@
 
 import SwiftUI
 
-struct SidebarPackageRow: View {
-
+struct SidebarPackageRow: View
+{
     let package: BrewPackage
 
     @AppStorage("allowMoreCompleteUninstallations") var allowMoreCompleteUninstallations: Bool = false
-    
+
     @AppStorage("enableRevealInFinder") var enableRevealInFinder: Bool = false
 
     @EnvironmentObject var appState: AppState
     @EnvironmentObject var brewData: BrewDataStorage
     @EnvironmentObject var outdatedPackageTracker: OutdatedPackageTracker
 
-    var body: some View {
-        NavigationLink {
+    var body: some View
+    {
+        NavigationLink
+        {
             PackageDetailView(package: package)
                 .id(package.id)
         } label: {
@@ -30,36 +32,52 @@ struct SidebarPackageRow: View {
         {
             contextMenuContent
         }
+        .swipeActions(edge: .leading, allowsFullSwipe: true)
+        {
+            tagUntagButton
+        }
+        .swipeActions(edge: .trailing, allowsFullSwipe: false)
+        {
+            UninstallPackageButton(package: package, isCalledFromSidebar: true)
+        }
     }
-    
+
     @ViewBuilder
-    var contextMenuContent: some View
+    var tagUntagButton: some View
     {
         Button
         {
-            Task(priority: .userInitiated) {
+            Task(priority: .userInitiated)
+            {
                 await changePackageTagStatus(
-                    package:package,
+                    package: package,
                     brewData: brewData,
                     appState: appState
                 )
             }
         } label: {
-            Text(package.isTagged ? "sidebar.section.all.contextmenu.untag-\(package.name)" : "sidebar.section.all.contextmenu.tag-\(package.name)")
+            Label(package.isTagged ? "sidebar.section.all.contextmenu.untag-\(package.name)" : "sidebar.section.all.contextmenu.tag-\(package.name)", systemImage: package.isTagged ? "tag.slash" : "tag")
         }
+    }
+    
+    @ViewBuilder
+    var contextMenuContent: some View
+    {
+        tagUntagButton
+        
         Divider()
-        
+
         UninstallPackageButton(package: package, isCalledFromSidebar: true)
-        
+
         if allowMoreCompleteUninstallations
         {
             PurgePackageButton(package: package, isCalledFromSidebar: true)
         }
-        
+
         if enableRevealInFinder
         {
             Divider()
-            
+
             Button
             {
                 do
